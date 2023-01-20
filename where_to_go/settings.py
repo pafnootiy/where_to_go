9934144de1b50c0fc0ff5f62224a73da526c16f1
pafnootiy <ksander-86@mail.ru>
2023-01-20 20:51:19 +0400

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -118,9 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print('STATIC_URL', STATIC_URL)
+print('STATICFILES_DIRS', STATICFILES_DIRS)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def show_main(request):
   
-    return render(request, 'main.html')
+    return render(request, 'index.html')
