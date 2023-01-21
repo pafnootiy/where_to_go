ab3c67f8c24873a0a0f331b94fff237a54c5648b
pafnootiy <ksander-86@mail.ru>
2023-01-21 04:43:22 +0400

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-
+from adminsortable2.admin import SortableAdminMixin
 # Register your models here.
-from .models import Place
+from .models import Place,Image
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = (
-        "title",)
+    list_display = ("title",)
+
+
     # list_display = (
     #     "title", "image", "pure_phonenumber",'description_short','description_long','latitude',
     #     'longitude',)
 # admin.site.register(Place)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+
+    list_display = ["place",]
+    ordering = ('id',)
+
--- places/models.py
+++ places/models.py
@@ -3,11 +3,23 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     title = models.CharField(verbose_name="Заголовок",max_length =50)
-    image = models.ImageField(upload_to="",verbose_name="Изображение",blank=True)
+    # image = models.ImageField(upload_to="",verbose_name="Изображение",blank=True)
     description_short=models.TextField(verbose_name="Описание короткое")
     description_long = models.TextField(verbose_name="Описание полное")
     latitude = models.FloatField(verbose_name="Долгота")
     longitude = models.FloatField(verbose_name="Широта")
+
+    def __str__(self) :
+        return self.title
+
+class Image(models.Model):
+    place = models.ForeignKey(Place,on_delete=models.CASCADE,verbose_name='Локация',
+                              related_name='images',blank=True,null=True)
+    picture = models.ImageField(upload_to="",verbose_name="Изображение",blank=True)
+
+    # def __str__(self) :
+    #     return self.place
+
 
 
 
