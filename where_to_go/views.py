75a12d6d3daacb1855e39ab9dfc56cbba8e4fdef
pafnootiy <ksander-86@mail.ru>
2023-01-25 03:50:52 +0400

    move GeoJason from view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -3,37 +3,29 @@ from django.shortcuts import render
 from places.models import Place, Image
 
 
+def convert_in_json(location):
+    serialized_location = {"type": "Feature",
+                           "geometry": {
+                               "type": "Point",
+                               "coordinates": [location.latitude, location.longitude]
+                           },
+                           "properties": {
+                               "title": location.title,
+                               "detailsUrl": ""
+                           }
+                           }
+    return serialized_location
+
 def index(request):
-    context={}
-    context['places'] ={
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "./static/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "./static/places/roofs24.json"
-          }
-        }
-      ]
-    }
+    locations = Place.objects.all()
+    context = {}
+    for location in locations:
 
-    return render(request, 'index.html',context)
+        context['places'] = {
+            "type": "FeatureCollection",
+            "features": [convert_in_json(location) for location in locations]}
+            
 
+        print(context)
+
+        return render(request, 'index.html', context)
