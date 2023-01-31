5246fd185d5267712158ae4d1f45862e3b9eab08
pafnootiy <ksander-86@mail.ru>
2023-02-01 02:25:29 +0400

    fix setting

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, )
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
-# MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
 # STATIC_ROOT = os.getenv("STATIC_ROOT")
 
