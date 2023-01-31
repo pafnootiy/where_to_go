6ccddf17d157db2f0f4c65817b614b02319a9225
pafnootiy <ksander-86@mail.ru>
2023-02-01 03:09:15 +0400

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, )
-MEDIA_ROOT = os.path.join(BASE_DIR,  )
+# MEDIA_ROOT = os.path.join(BASE_DIR,  )
  
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
