7c741dabde6821e7044ca5a80d4213b36518c120
pafnootiy <ksander-86@mail.ru>
2023-02-02 11:46:00 +0400

    update code with pep8

--- places/admin.py
+++ places/admin.py
@@ -1,36 +1,35 @@
+from adminsortable2.admin import SortableAdminBase
+from adminsortable2.admin import SortableAdminMixin
+from adminsortable2.admin import SortableTabularInline
 from django.contrib import admin
 from django.utils.safestring import mark_safe
- 
-from .models import Place,Image
-from adminsortable2.admin import SortableAdminMixin
- 
-from adminsortable2.admin import SortableAdminBase
-from adminsortable2.admin import SortableTabularInline
- 
+
+from .models import Place, Image
+
 
 class ImageInline(SortableTabularInline):
-
     model = Image
-    extra  =1
-    list_display = ("preview_image","place","position",)
+    extra = 1
+    list_display = ("preview_image", "place", "position",)
 
     readonly_fields = ("preview_image",)
+
     def preview_image(self, obj):
-        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-            url = obj.picture.url,
-            width="200",
-            height="120",
+        return mark_safe(
+            '<img src="{url}" width="{width}" height={height} />'.format(
+                url=obj.picture.url,
+                width="200",
+                height="120",
             )
         )
 
 
-
 @admin.register(Place)
-class PlaceAdmin(SortableAdminBase,admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     list_display = ("title",)
     inlines = [ImageInline]
 
 
 @admin.register(Image)
 class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ("position","place",)
+    list_display = ("position", "place",)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from places.models import Place, Image
-from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse
+
+from places.models import Place
 
 
 def get_serialized_json(location):
@@ -26,31 +26,34 @@ def convert_into_json(location):
     serialized_location = {"type": "Feature",
                            "geometry": {
                                "type": "Point",
-                               "coordinates": [location.longitude, location.latitude]
+                               "coordinates": [location.longitude,
+                                               location.latitude]
                            },
                            "properties": {
                                "title": location.title,
                                "placeId": location.id,
-                               "detailsUrl": reverse("location_info", kwargs={'pk': location.id})
+                               "detailsUrl": reverse("location_info", kwargs={
+                                   'pk': location.id})
                            }
                            }
-                        
-    return serialized_location
 
+    return serialized_location
 
 
 def json_api(request, pk):
     location = get_object_or_404(Place, id=pk)
 
-    return JsonResponse(get_serialized_json(location), safe=False, json_dumps_params={
-           'ensure_ascii': False, "indent": 2})
+    return JsonResponse(get_serialized_json(location), safe=False,
+                        json_dumps_params={
+                            'ensure_ascii': False, "indent": 2})
 
 
 def index(request):
     locations = Place.objects.all()
     context = {}
     for location in locations:
-        context['locations'] = { 
+        context['locations'] = {
             "type": "FeatureCollection",
-            "features": [convert_into_json(location) for location in locations]}
+            "features": [convert_into_json(location) for location in
+                         locations]}
     return render(request, 'index.html', context)
